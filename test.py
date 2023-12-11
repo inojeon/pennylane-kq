6a1fa168bc7a8e13f722d579aaa5d0a0d93adc14
ino <ino@kakao.com>
2023-12-12 00:12:01 +0900

    api server 연동

--- test.py
+++ test.py
@@ -5,16 +5,24 @@ secretAccessKey = "BhPxW18nhxLuB65sYJ6lYMywQPS3PM7QFgsIML8e26I="
 
 
 dev = qml.device(
-    "kq.emulator", accessKeyId=accessKeyId, secretAccessKey=secretAccessKey
+    "kq.emulator",
+    wires=2,
+    shots=2048,
+    accessKeyId=accessKeyId,
+    secretAccessKey=secretAccessKey,
 )
 
 
 @qml.qnode(dev)
 def circuit(x, y):
-    qml.RX(x, wires=0)
-    qml.RY(y, wires=0)
-    return qml.counts(qml.PauliZ(0))
+    qml.Hadamard(wires=0)
+    qml.CNOT(wires=[0, 1])
+    qml.Hadamard(wires=1)
+    # qml.RX(x, wires=0)
+    # qml.RY(y, wires=0)
+    return qml.counts()
 
 
 result = circuit(0, 0)
 print(result)
+# print(dev._circuit.qasm(formatted=True))
